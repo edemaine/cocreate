edemaine/cocreate

commit 259620e612181a0c6168d4ec8fc8a10943cd1983

Compress exported SVG slightly using self-closing tags

client/tools/download.coffee
@@ -35,6 +35,8 @@ defineTool
       .replace /</g, '&lt;'
       .replace />/g, '&gt;'
       }\""
+    ## Compress using SVG's self-closing tags
+    .replace ///(<(\w+)\b[^<>]*)> \s* </\2>///g, '$1/>'
     ## Reset transform and grid
     root.setAttribute 'transform', oldTransform if oldTransform?
     grid?.update()

commit 6a2a4ba1ea31c654d063a3bd7a908163fffd5285

Fix SVG compression to not eat thin spaces
Bug introduced in 259620e612181a0c6168d4ec8fc8a10943cd1983

client/tools/download.coffee
@@ -36,7 +36,7 @@ defineTool
       .replace />/g, '&gt;'
       }\""
     ## Compress using SVG's self-closing tags
-    .replace ///(<(\w+)\b[^<>]*)> \s* </\2>///g, '$1/>'
+    .replace ///(<(\w+)\b[^<>]*)> </\2>///g, '$1/>'
     ## Remove selection-helping rect.bbox elements from text objects
     .replace ///<rect [^<>]* class="bbox" [^<>]*/>///g, ''
     ## Reset transform and grid